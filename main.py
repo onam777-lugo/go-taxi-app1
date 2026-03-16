c82455815519d1ba09034491dfbfcf76ba8a57e8
onam777-lugo <manoogul56@gmail.com>
2026-03-16 18:15:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -153,7 +153,7 @@ try:
                     """, unsafe_allow_html=True)
 
                     if not bloquear:
-                        with st.expander("VER OPCIONES DE VIAJE"):
+                        with st.expander("CONTACTAR"):
                             st.markdown("**💳 PAGO MÓVIL / DATOS:**")
                             st.code(pago, language=None) 
                             st.markdown("---")
