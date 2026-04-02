2364d2a3349da20e9aeb05fdb97805d1ada98e9a
¡Go! TAXI <manoogul56@gmail.com>
2026-04-01 22:26:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ st.set_page_config(
 )
 
 # --- AJUSTE DE TRANSPARENCIA (Cambia este valor del 0.0 al 1.0) ---
-opacidad = 0.85 
+opacidad = 0.5 
 
 # 2. ESTILOS BASE (CON SOMBRAS Y TRANSPARENCIAS)
 st.markdown(f"""
