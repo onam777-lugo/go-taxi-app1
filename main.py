b2e1c390fdf826f4b655d8be4705d5e883f0f2ab
¡Go! TAXI <manoogul56@gmail.com>
2026-04-18 11:24:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ st.markdown(f"""
     /* CABECERA NARANJA SIN BORDES (PANTALLA COMPLETA) */
     .header-curva {{
         background-color: #FF8C00;
-        height: 320px;
+        height: 380px;
         position: absolute;
         top: -100px;
         left: -100vw; /* Extensión masiva a los lados */
